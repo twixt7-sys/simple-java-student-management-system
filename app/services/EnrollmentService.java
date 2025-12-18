f35e2e05839fdd6bdaf8570360bea5fd3f4d8492
bloo <nexushellstein@gmail.com>
2025-12-18 15:44:24 +0800

    established CourseService

---
+++ app/services/EnrollmentService.java
@@ -0,0 +1,12 @@
+package services;
+
+import models.Enrollment;
+
+public class EnrollmentService {
+
+    public void enrollStudent(Enrollment enrollment) {
+    }
+
+    public void removeEnrollment(int enrollmentId) {
+    }
+}
