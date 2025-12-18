e8c93577651c18b7b5c3065d7535c15ea3a152e8
bloo <nexushellstein@gmail.com>
2025-12-18 15:43:01 +0800

    established StudentService

---
+++ app/services/StudentService.java
@@ -0,0 +1,15 @@
+package app.services;
+
+import app.models.Student;
+
+public class StudentService {
+
+    public void createStudent(Student student) {
+    }
+
+    public void updateStudent(Student student) {
+    }
+
+    public void deleteStudent(int studentId) {
+    }
+}
