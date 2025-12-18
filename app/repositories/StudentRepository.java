592dd74f8e8114e03c809607ae99764aaa8dbfa8
bloo <nexushellstein@gmail.com>
2025-12-18 15:34:30 +0800

    established StudentRepository

---
+++ app/repositories/StudentRepository.java
@@ -0,0 +1,24 @@
+package app.repositories;
+
+import app.models.Student;
+import java.util.List;
+
+public class StudentRepository {
+
+    public void addStudent(Student student) {
+    }
+
+    public void updateStudent(Student student) {
+    }
+
+    public void deleteStudent(int studentId) {
+    }
+
+    public Student getStudentById(int studentId) {
+        return null;
+    }
+
+    public List<Student> getAllStudents() {
+        return null;
+    }
+}
