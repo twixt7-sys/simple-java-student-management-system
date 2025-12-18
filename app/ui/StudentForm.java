70a97c88eb007357ddfcacb966d5d1f4b31d2ee4
bloo <nexushellstein@gmail.com>
2025-12-18 15:59:56 +0800

    established ui StudentForm

---
+++ app/ui/StudentForm.java
@@ -0,0 +1,13 @@
+package app.ui;
+
+import javax.swing.JPanel;
+
+public class StudentForm extends JPanel {
+
+    public StudentForm() {
+        initialize();
+    }
+
+    private void initialize() {
+    }
+}
