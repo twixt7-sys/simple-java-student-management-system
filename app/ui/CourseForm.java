9a3d2e02cbc197fcff3949d327fb087bf3ddf729
bloo <nexushellstein@gmail.com>
2025-12-18 16:01:22 +0800

    established ui CourseForm

---
+++ app/ui/CourseForm.java
@@ -0,0 +1,13 @@
+package app.ui;
+
+import javax.swing.JPanel;
+
+public class CourseForm extends JPanel {
+
+    public CourseForm() {
+        initialize();
+    }
+
+    private void initialize() {
+    }
+}
