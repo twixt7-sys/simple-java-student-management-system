21d4796efe1943f81fd914d7de0f1a2b3d44fb42
bloo <nexushellstein@gmail.com>
2025-12-18 16:02:29 +0800

    established ui EnrollmentForm

---
+++ app/ui/EnrollmentForm.java
@@ -0,0 +1,13 @@
+package app.ui;
+
+import javax.swing.JPanel;
+
+public class EnrollmentForm extends JPanel {
+
+    public EnrollmentForm() {
+        initialize();
+    }
+
+    private void initialize() {
+    }
+}
