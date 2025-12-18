0c07705aa4d553e131b2980d37b21226348204d3
bloo <nexushellstein@gmail.com>
2025-12-18 15:37:10 +0800

    established EnrollmentRepository

---
+++ app/repositories/EnrollmentRepository.java
@@ -0,0 +1,17 @@
+package app.repositories;
+
+import app.models.Enrollment;
+import java.util.List;
+
+public class EnrollmentRepository {
+
+    public void addEnrollment(Enrollment enrollment) {
+    }
+
+    public void deleteEnrollment(int enrollmentId) {
+    }
+
+    public List<Enrollment> getEnrollmentsByStudent(int studentId) {
+        return null;
+    }
+}
