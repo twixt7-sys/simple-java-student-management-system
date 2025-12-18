1b09fe338bd7a52f901331aeb0ab11ba60d10cfc
bloo <nexushellstein@gmail.com>
2025-12-18 15:35:39 +0800

    established CourseRepository

---
+++ app/repositories/CourseRepository.java
@@ -0,0 +1,24 @@
+package app.repositories;
+
+import app.models.Course;
+import java.util.List;
+
+public class CourseRepository {
+
+    public void addCourse(Course course) {
+    }
+
+    public void updateCourse(Course course) {
+    }
+
+    public void deleteCourse(int courseId) {
+    }
+
+    public Course getCourseById(int courseId) {
+        return null;
+    }
+
+    public List<Course> getAllCourses() {
+        return null;
+    }
+}
