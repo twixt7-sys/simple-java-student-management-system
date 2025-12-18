22237aa7f84d8e66cf51ae19e972f9da87cb8b85
bloo <nexushellstein@gmail.com>
2025-12-18 15:43:45 +0800

    established CourseService

---
+++ app/services/CourseService.java
@@ -0,0 +1,15 @@
+package app.services;
+
+import app.models.Course;
+
+public class CourseService {
+
+    public void createCourse(Course course) {
+    }
+
+    public void updateCourse(Course course) {
+    }
+
+    public void deleteCourse(int courseId) {
+    }
+}
