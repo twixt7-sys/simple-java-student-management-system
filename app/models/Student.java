09fadcc2fa35e9c6a1006214698bd0eafee626ad
bloo <nexushellstein@gmail.com>
2025-12-18 15:23:17 +0800

    established Student model

---
+++ app/models/Student.java
@@ -0,0 +1,32 @@
+package app.models;
+
+public class Student extends Person {
+
+    private String studentNumber;
+    private String program;
+
+    public Student() {}
+
+    public Student(int id, String firstName, String lastName, String email,
+                   String studentNumber, String program) {
+        super(id, firstName, lastName, email);
+        this.studentNumber = studentNumber;
+        this.program = program;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+}
