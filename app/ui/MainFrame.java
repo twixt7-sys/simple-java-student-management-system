245380e922a300743516a76a6af3ebe816e0f346
bloo <nexushellstein@gmail.com>
2025-12-18 15:58:39 +0800

    minor changes

---
+++ app/ui/MainFrame.java
@@ -0,0 +1,13 @@
+package app.ui;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        initialize();
+    }
+
+    private void initialize() {
+    }
+}
