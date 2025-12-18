e55d6b5d898be6f0e3a1f7252fd521077f047150
bloo <nexushellstein@gmail.com>
2025-12-18 15:55:24 +0800

    established Validator util

---
+++ app/utils/Validator.java
@@ -0,0 +1,12 @@
+package app.utils;
+
+public class Validator {
+
+    public static boolean isNullOrEmpty(String value) {
+        return false;
+    }
+
+    public static boolean isValidEmail(String email) {
+        return false;
+    }
+}
